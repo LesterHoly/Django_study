7846f19baa10330ba641dc6da56ab3dee5826e68
lester <houlei0911@gmail.com>
2020-02-11 11:02:18 +0800

    add user create module

--- myProject/myproject/accounts/views.py
+++ myProject/myproject/accounts/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import UserCreationForm
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
